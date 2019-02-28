5fca0bd432e0e1380c1e57e0da5aa71147c848b6
fatihsaracoglu <fatihsaracoglu@outlook.com>
2019-02-28 22:55:23 +0300

    Added 'Guess Number' exercise

---
+++ GuessNumber/GuessNumber.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+
+	public static void main(String[] args) throws IOException {	
+		Scanner reader = new Scanner(System.in); //Creates an object to read user input
+		Random rand = new Random(); //Creates an object from Random class
+		int number =rand.nextInt(100); //generates a number between 0 and 99
+		
+		System.out.println("Hi! I'm thinking of a number between 0 and 99.");
+		System.out.print("Can you guess it: ");
+		
+		int guess = reader.nextInt(); //Read the user input
+
+        int counter = 1;
+
+        while (guess != number && guess != -1) {
+            System.out.println("Sorry!");
+            if (number > guess) {
+                System.out.println("Mine is greater than your guess.");
+            } else {
+                System.out.println("Mine is less than your guess.");
+            }
+            System.out.print("Type -1 to quit or guess another: ");
+            guess = reader.nextInt();
+            counter++;
+        }
+
+        if (number == guess) {
+            System.out.println("Congratulations! You won after " + counter + " attempts!");
+        } else {
+            System.out.println("Sorry, the number was " + number);
+        }
+
+		reader.close(); //Close the resource before exiting
+	}
+}
